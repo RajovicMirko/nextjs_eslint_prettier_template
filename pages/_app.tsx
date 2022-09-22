c1e6c3333b3e87ee6dd512a1743d6641d1113eae
MirkoRajovicSY <mirko.rajovic@symphony.is>
2022-09-22 07:58:01 +0200

    Add test pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div
+      style={{
+        position: 'relative',
+        minHeight: '100vh',
+        background: 'pink'
+      }}
+    >
+      <div>
+        <h1>Test</h1>
+
+        <div style={{ display: 'flex', columnGap: '20px' }}>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/car">
+            <a>Car</a>
+          </Link>
+        </div>
+
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
 export default MyApp;
---
+++ pages/car/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const Car: NextPage = () => {
+  return <div>Car page</div>;
+};
+
+export default Car;
